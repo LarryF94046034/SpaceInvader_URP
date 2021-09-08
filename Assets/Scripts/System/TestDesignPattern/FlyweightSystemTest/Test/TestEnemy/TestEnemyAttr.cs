c62982082a6ed2d62ee98130b7a7ea0765449a67
DESKTOP-PJCOQV5\user <larry110864@gmail.com>
2021-09-09 06:13:00 +0800

    Level3打開Timeline，顯示prefab bullet

--- Assets/Scripts/System/TestDesignPattern/FlyweightSystemTest/Test/TestEnemy/TestEnemyAttr.cs
+++ Assets/Scripts/System/TestDesignPattern/FlyweightSystemTest/Test/TestEnemy/TestEnemyAttr.cs
@@ -29,28 +29,7 @@ public class TestEnemyAttr : MonoBehaviour
         sodierAttrSO.m_CritRate=tmpAttr.GetCritRate();
 
 
-        //測試精靈王
-
-        // EnemyAttr sodierAttr=attrFactory.GetEnemyAttr(11);
-
-        // Debug.Log(sodierAttr.GetMaxHP());
-        // Debug.Log(sodierAttr.GetMoveSpeed());
-        // Debug.Log(sodierAttr.GetAttrName());
-        // Debug.Log(sodierAttr.GetCritRate());
-        // Debug.Log(sodierAttrSO);
-        // sodierAttrSO.m_MaxHP=sodierAttr.GetMaxHP();
-        // sodierAttrSO.m_MoveSpeed=sodierAttr.GetMoveSpeed();
-        // sodierAttrSO.m_AttrName=sodierAttr.GetAttrName();
-
-        // EnemyAttr tmpAttr=attrFactory.GetEliteEnemyAttr(ENUM_AttrDecorator.EnemyPrefix,41,sodierAttr);
-
-        // sodierAttrSO.m_MaxHP=tmpAttr.GetMaxHP();
-        // sodierAttrSO.m_MoveSpeed=tmpAttr.GetMoveSpeed();
-        // sodierAttrSO.m_AttrName=tmpAttr.GetAttrName();
-
-        // sodierAttrSO.m_CritRate=tmpAttr.GetCritRate();
-        // sodierAttrSO.m_MissRate=tmpAttr.GetMissRate();
-        // sodierAttrSO.m_RecoverIncreaseRate=tmpAttr.GetRecoverIncreaseRate();
+        
         
 
     }
